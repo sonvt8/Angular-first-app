a137dc74bdfc93506b040da3a1591995761e58b3
Thai Son Vu <vuthaison198x@gmail.com>
2021-04-16 09:14:28 +0700

    add shopping list using service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
+import { Ingredient } from '../shared/ingredients.model';
+import {EventEmitter} from "@angular/core";
+
 export class ShoppingListService {
-    
+    ingredientsChanged= new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Eggs', 4),
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice())
+    }
 }
