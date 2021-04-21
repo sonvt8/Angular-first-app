b2bb2a398f7ed39a61d3177b2b8b42cafd0cf590
Thai Son Vu <vuthaison198x@gmail.com>
2021-04-21 11:50:41 +0700

    make route to edit recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RouterModule } from '@angular/router';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
-    {path: '', component: RecipeStartComponent}
+    {path: '', component: RecipeStartComponent},
+    {path: 'new', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent},
   ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ];
