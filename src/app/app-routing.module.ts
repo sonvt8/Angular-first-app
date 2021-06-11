9b4d203747afee18ece218a11d46c7e3a233a06b
Thai Son Vu <vuthaison198x@gmail.com>
2021-06-11 15:37:58 +0700

    add guard to resolve data before loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { RouterModule } from '@angular/router';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 
 const appRoutes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: RecipeStartComponent},
     {path: 'new', component: RecipeEditComponent},
-    {path: ':id', component: RecipeDetailComponent},
-    {path: ':id/edit', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]}, //Resole help to fetch data from FireBase before going to recipe detail
+    {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}, //Resole help to fetch data from FireBase before going to recipe detail
   ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ];
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -19,14 +19,16 @@ export class DataStorageService {
   }
 
   fetchRecipes(){
-    this.http.get<Recipe[]>('https://angular-firstapp-ce05c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
-    .pipe(map(recipes => {
-      return recipes.map(recipe => {
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-      });
-    }))
-    .subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
-    })
+    return this.http.get<Recipe[]>('https://angular-firstapp-ce05c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
+    .pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    )
   }
 }
