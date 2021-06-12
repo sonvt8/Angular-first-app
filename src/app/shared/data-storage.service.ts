8d2632810b9a4e4a9eca7f5ac6d07d5c8f396523
Thai Son Vu <vuthaison198x@gmail.com>
2021-06-12 20:52:47 +0700

    fetch data after authentication

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {map, tap} from 'rxjs/operators';
+import {map, tap, take, exhaustMap} from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -9,7 +10,7 @@ import { RecipeService } from '../recipes/recipe.service';
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes(){
     const  recipes = this.recipeService.getRecipes();
@@ -19,8 +20,16 @@ export class DataStorageService {
   }
 
   fetchRecipes(){
-    return this.http.get<Recipe[]>('https://angular-firstapp-ce05c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
-    .pipe(
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap(user => {
+        return this.http.get<Recipe[]>(
+          'https://angular-firstapp-ce05c-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token)
+          }
+        );
+      }),
       map(recipes => {
         return recipes.map(recipe => {
           return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
