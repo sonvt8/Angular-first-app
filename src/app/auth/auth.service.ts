67020c1a9c696191fffcd6e9348d39720f07d170
Thai Son Vu <vuthaison198x@gmail.com>
2021-06-12 16:31:34 +0700

    handle login method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs'
 
-interface AuthResponseData {
+export interface AuthResponseData {
   email: string;
   idToken: string;
   refreshToken: string;
   kind: string;
   expiresIn: string;
   localId: string;
+  registered?: string;
 }
 
 @Injectable({
@@ -26,16 +27,35 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     })
-    .pipe(catchError(errorRes => {
-      let errorMess = 'An unknown error occured!';
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMess);
-      }
-      switch(errorRes.error.error.message){
-        case 'EMAIL_EXISTS':
-          errorMess = 'this email exists aldready'
-      }
+    .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDrdPu0n_7EoFXqIIwOfGsZFHzcPekPWqc',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    })
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMess = 'An unknown error occured!';
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMess);
-    }));
+    }
+    switch(errorRes.error.error.message){
+      case 'EMAIL_EXISTS':
+        errorMess = 'this email exists aldready'
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMess = 'this email does not exist.'
+        break;
+      case 'INVALID_PASSWORD':
+        errorMess = 'Invalid password.'
+        break;
+    }
+    return throwError(errorMess);
   }
 }
